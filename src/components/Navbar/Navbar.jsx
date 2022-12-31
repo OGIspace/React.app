0cac0125958cbc945f7d8ece3fa9099594d3af59
OGIspace <jonesjefferfon@gmail.com>
2022-12-31 16:03:37 +0200

    123

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -46,22 +46,7 @@ const Navbar = () => {
       ? setToggleIcon("toggler__icon toggle")
       : setToggleIcon("toggler__icon");
   };
-  var modal = document.getElementById("myModal");
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-      btn.onclick = function() {
-        modal.style.display = "block";
-      }
 
-      span.onclick = function() {
-        modal.style.display = "none";
-      }
-
-      window.onclick = function(event) {
-        if (event.target === modal) {
-          modal.style.display = "none";
-        }
-      }
   return (
     <>
     <div className="nav__wrapper">
